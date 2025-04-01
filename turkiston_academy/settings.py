25a73ad7364c77bd951b354ed573c7315dddf712
KMohir <komilovmohirbekfda11@gmail.com>
2025-04-01 20:47:59 +0500

    fix settings

--- turkiston_academy/settings.py
+++ turkiston_academy/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-=&mb#ljmx+6=dc5jd+i#s%t5p-&b+fxeu_5vomdnwnh9f!2-y&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["turkistonakademiyasi.uz"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -80,24 +80,24 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Database
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'turk_akademiya',
-#         'USER': 'postgres',
-#         'PASSWORD': '111',
-#         'HOST': 'localhost',  # если сервер PostgreSQL находится на той же машине, где и Django
-#         'PORT': '5432',  # стандартный порт PostgreSQL
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'turk_akademiya',
+        'USER': 'postgres',
+        'PASSWORD': '111',
+        'HOST': 'localhost',  # если сервер PostgreSQL находится на той же машине, где и Django
+        'PORT': '5432',  # стандартный порт PostgreSQL
+    }
+}
 
 
 # Password validation
@@ -132,7 +132,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Добавьте эту строкуы
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://turkistonakademiyasi.uz",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CSRF_COOKIE_SECURE = True
